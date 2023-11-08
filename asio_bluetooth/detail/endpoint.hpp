1e9f9be4e52b753d9ecd8a609b5d16f3ba8558b7
Amr Elsayyad <amrelsayyad96@gmail.com>
2023-11-08 15:49:59 +0200

    Fix str2ba error

--- asio_bluetooth/detail/endpoint.hpp
+++ asio_bluetooth/detail/endpoint.hpp
@@ -4,7 +4,6 @@
 //  email: dan0702@g.ucla.edu
 //
 
-
 #ifndef BOOST_ASIO_BLUETOOTH_DETAIL_ENDPOINT_HPP
 #define BOOST_ASIO_BLUETOOTH_DETAIL_ENDPOINT_HPP
 
@@ -12,171 +11,154 @@
 #include <boost/asio/detail/socket_types.hpp>
 #include <boost/asio/detail/winsock_init.hpp>
 #include <boost/system/error_code.hpp>
-
-
 #include <cstddef>
 #include <string>
 
 // misc, type definitions
-#include "../misc.hpp"
-
 #include <boost/asio/detail/push_options.hpp>
+
+#include "../misc.hpp"
 
 namespace boost {
 namespace asio {
 namespace bluetooth {
 namespace detail {
 
-class endpoint
-{
-public:
-  // Default constructor
-  endpoint()
-    : data_()
-  {
-    data_.bt.rc_family = AF_BLUETOOTH;
-    data_.bt.rc_bdaddr = (bdaddr_t) { 0 };
-    data_.bt.rc_channel = (uint8_t) 1;
-    // TODO can we figure out the BT mac of the local host?
-    mac_ = "[BDADDR_ANY]";
-  }
-
-  endpoint(uint8_t channel)
-    : data_()
-  {
-    data_.bt.rc_family = AF_BLUETOOTH;
-    data_.bt.rc_bdaddr = (bdaddr_t) { 0 };
-    data_.bt.rc_channel = (uint8_t) channel;
-    // TODO can we figure out the BT mac of the local host?
-    mac_ = "[BDADDR_ANY]";
-  }
-
-  // Construct endpoint using bt MAC address and channel number
-  endpoint(const char* mac_addr, uint8_t channel)
-    : data_()
-  {
-    data_.bt.rc_family = AF_BLUETOOTH;
-    char addr[18];
-    memcpy(addr, mac_addr, 18);
-    str2ba( addr, &data_.bt.rc_bdaddr );
-    data_.bt.rc_channel = (uint8_t) channel;
-
-    mac_ = std::string(mac_addr);
-  }
-
-  // Constructor using std::string
-  endpoint(const std::string& mac_addr, uint8_t channel)
-    : data_()
-  {
-    data_.bt.rc_family = AF_BLUETOOTH;
-    char addr[18];
-    memcpy(addr, mac_addr.c_str(), 17);
-    addr[17] = 0;
-    std::istringstream ss(addr);
-    for (int i = 0; i < 6; i++) {
-        int x;
-        ss >> std::hex >> x;
-        data_.bt.rc_bdaddr.b[i] = static_cast<uint8_t>(x);
-        if (i < 5) ss.ignore(1); // skip the colon
+class endpoint {
+  public:
+    // Default constructor
+    endpoint() : data_() {
+        data_.bt.rc_family = AF_BLUETOOTH;
+        data_.bt.rc_bdaddr = (bdaddr_t){0};
+        data_.bt.rc_channel = (uint8_t)1;
+        // TODO can we figure out the BT mac of the local host?
+        mac_ = "[BDADDR_ANY]";
     }
-    data_.bt.rc_channel = (uint8_t) channel;
 
-    mac_ = mac_addr;
-  }
-
-  // Copy Constructor
-  endpoint(const endpoint& other)
-    : data_(other.data_)
-  {
-  }
-
-  // Assigned from another endpoint
-  endpoint& operator=(const endpoint& other)
-  {
-    data_ = other.data_;
-    return *this;
-  }
-
-  // Get the underlying endpoint in the native type
-  boost::asio::detail::socket_addr_type* data()
-  {
-    return &data_.base;
-  }
-
-  // Get the underlying endpoint in the native type
-  const boost::asio::detail::socket_addr_type* data() const
-  {
-    return &data_.base;
-  }
-
-  // Get the underlying size of the endpoint in the native type
-  std::size_t size() const
-  {
-    return sizeof(boost::asio::detail::sockaddr_bt_type);
-  }
-
-  friend bool operator==(const endpoint& e1, const endpoint& e2)
-  {
-    if (e1.address() != e2.address()) {
-      return false;
+    endpoint(uint8_t channel) : data_() {
+        data_.bt.rc_family = AF_BLUETOOTH;
+        data_.bt.rc_bdaddr = (bdaddr_t){0};
+        data_.bt.rc_channel = (uint8_t)channel;
+        // TODO can we figure out the BT mac of the local host?
+        mac_ = "[BDADDR_ANY]";
     }
-    if (e1.channel() != e1.channel()) {
-      return false;
+
+    // Construct endpoint using bt MAC address and channel number
+    endpoint(const char* mac_addr, uint8_t channel) : data_() {
+        data_.bt.rc_family = AF_BLUETOOTH;
+        char addr[18];
+        memcpy(addr, mac_addr, 18);
+        str2ba(addr, &data_.bt.rc_bdaddr);
+        data_.bt.rc_channel = (uint8_t)channel;
+
+        mac_ = std::string(mac_addr);
     }
-    return true;
-  }
 
-  friend bool operator<(const endpoint& e1, const endpoint& e2)
-  {
-    return e1.address() < e2.address();
-  }
-  
-  // Set the underlying size of the endpoint in the native type
-  void resize(std::size_t new_size)
-  {
-    if (new_size > sizeof(boost::asio::detail::sockaddr_storage_type))
-    {
-      boost::system::error_code ec(boost::asio::error::invalid_argument);
-      boost::asio::detail::throw_error(ec);
+    // Constructor using std::string
+    endpoint(const std::string& mac_addr, uint8_t channel) : data_() {
+        data_.bt.rc_family = AF_BLUETOOTH;
+        char addr[18];
+        memcpy(addr, mac_addr.c_str(), 17);
+        addr[17] = 0;
+        str2ba(addr, &data_.bt.rc_bdaddr);
+        data_.bt.rc_channel = (uint8_t)channel;
+
+        mac_ = mac_addr;
     }
-  }
 
-  // Get the capacity of the endpoint in the native type
-  std::size_t capacity() const
-  {
-    return sizeof(boost::asio::detail::sockaddr_bt_type);
-  }
+    // Copy Constructor
+    endpoint(const endpoint& other) : data_(other.data_) {}
 
-  // Get Bluetooth MAC address
-  std::string address() const
-  {
-    // TODO: convert directly from the address structure
-    return mac_;
-  }
+    // Assigned from another endpoint
+    endpoint& operator=(const endpoint& other) {
+        data_ = other.data_;
+        return *this;
+    }
 
-  // Get the Bluetooth channel
-  uint8_t channel() const
-  {
-    return (uint8_t) data_.bt.rc_channel;
-  }
+    // Get the underlying endpoint in the native type
+    boost::asio::detail::socket_addr_type* data() { return &data_.base; }
 
-private:
-  union data_union
-  {
-    // base address type for most of the address types
-    // struct sockaddr
-    boost::asio::detail::socket_addr_type base;
-    boost::asio::detail::sockaddr_bt_type bt;
-  } data_;
+    // Get the underlying endpoint in the native type
+    const boost::asio::detail::socket_addr_type* data() const {
+        return &data_.base;
+    }
 
-  std::string mac_;
+    // Get the underlying size of the endpoint in the native type
+    std::size_t size() const {
+        return sizeof(boost::asio::detail::sockaddr_bt_type);
+    }
+
+    friend bool operator==(const endpoint& e1, const endpoint& e2) {
+        if (e1.address() != e2.address()) {
+            return false;
+        }
+        if (e1.channel() != e1.channel()) {
+            return false;
+        }
+        return true;
+    }
+
+    friend bool operator<(const endpoint& e1, const endpoint& e2) {
+        return e1.address() < e2.address();
+    }
+
+    // Set the underlying size of the endpoint in the native type
+    void resize(std::size_t new_size) {
+        if (new_size > sizeof(boost::asio::detail::sockaddr_storage_type)) {
+            boost::system::error_code ec(boost::asio::error::invalid_argument);
+            boost::asio::detail::throw_error(ec);
+        }
+    }
+
+    // Get the capacity of the endpoint in the native type
+    std::size_t capacity() const {
+        return sizeof(boost::asio::detail::sockaddr_bt_type);
+    }
+
+    // Get Bluetooth MAC address
+    std::string address() const {
+        // TODO: convert directly from the address structure
+        return mac_;
+    }
+
+    // Get the Bluetooth channel
+    uint8_t channel() const { return (uint8_t)data_.bt.rc_channel; }
+
+  private:
+    void baswap(bdaddr_t* dst, const bdaddr_t* src) {
+        unsigned char* d = (unsigned char*)dst;
+        const unsigned char* s = (const unsigned char*)src;
+        int i;
+        for (i = 0; i < 6; i++) d[i] = s[5 - i];
+    }
+
+    int str2ba(const char* str, bdaddr_t* ba) {
+        uint8_t b[6];
+        const char* ptr = str;
+        int i;
+        for (i = 0; i < 6; i++) {
+            b[i] = (uint8_t)strtol(ptr, NULL, 16);
+            if (i != 5 && !(ptr = strchr(ptr, ':'))) ptr = ":00:00:00:00:00";
+            ptr++;
+        }
+        baswap(ba, (bdaddr_t*)b);
+        return 0;
+    }
+
+    union data_union {
+        // base address type for most of the address types
+        // struct sockaddr
+        boost::asio::detail::socket_addr_type base;
+        boost::asio::detail::sockaddr_bt_type bt;
+    } data_;
+
+    std::string mac_;
 };
 
-
-} // namespace detail
-} // namespace bluetooth
-} // namespace asio
-} // namespace boost
+}  // namespace detail
+}  // namespace bluetooth
+}  // namespace asio
+}  // namespace boost
 
 #include <boost/asio/detail/pop_options.hpp>
 
